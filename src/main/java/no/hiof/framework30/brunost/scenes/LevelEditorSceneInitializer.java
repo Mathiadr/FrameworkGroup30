864ba3273bef62b7ae3f0f740b532c044cb8e190
Mathiadr <mathiadr@hiof.no>
2023-04-10 19:43:07 +0200

    Improved example sprites

--- src/main/java/no/hiof/framework30/brunost/scenes/LevelEditorSceneInitializer.java
+++ src/main/java/no/hiof/framework30/brunost/scenes/LevelEditorSceneInitializer.java
@@ -15,6 +15,7 @@ import org.joml.Vector2f;
 // Source: GamesWithGabe, 27.09.21 - https://www.youtube.com/playlist?list=PLtrSb4XxIVbp8AKuEAlwNXDxr99e3woGE
 public class LevelEditorSceneInitializer extends SceneInitializer {
     private SpriteSheet spritesSpritesheet;
+    private SpriteSheet defaultTilesSpritesheet;
     private GameObject levelEditorStuff;
 
     public LevelEditorSceneInitializer() {
@@ -23,8 +24,10 @@ public class LevelEditorSceneInitializer extends SceneInitializer {
 
     @Override
     public void init(Scene scene){
-        spritesSpritesheet = AssetPool.getSpriteSheet("assets/images/spritesheet.png");
+        spritesSpritesheet = AssetPool.getSpriteSheet("assets/images/RatGirlSpritesheet.png");
         SpriteSheet gizmos = AssetPool.getSpriteSheet("assets/images/gizmos.png");
+        defaultTilesSpritesheet = AssetPool.getSpriteSheet("assets/images/defaultTiles.png");
+
 
         levelEditorStuff = scene.createGameObject("LevelEditor");
         levelEditorStuff.setNoSerialization();
@@ -40,9 +43,12 @@ public class LevelEditorSceneInitializer extends SceneInitializer {
     public void loadResources(Scene scene){
         AssetPool.getShader("assets/shaders/default.glsl");
 
-        AssetPool.addSpriteSheet("assets/images/spritesheet.png",
-                new SpriteSheet(AssetPool.getTexture("assets/images/spritesheet.png"),
-                1000, 1000, 8, 0));
+        AssetPool.addSpriteSheet("assets/images/RatGirlSpritesheet.png",
+                new SpriteSheet(AssetPool.getTexture("assets/images/RatGirlSpritesheet.png"),
+                70, 75, 8, 0));
+        AssetPool.addSpriteSheet("assets/images/defaultTiles.png",
+                new SpriteSheet(AssetPool.getTexture("assets/images/defaultTiles.png"),
+                64, 64, 4, 1));
         AssetPool.addSpriteSheet("assets/images/gizmos.png",
                 new SpriteSheet(AssetPool.getTexture("assets/images/gizmos.png"),
                         24, 48, 3, 0));
@@ -83,8 +89,8 @@ public class LevelEditorSceneInitializer extends SceneInitializer {
         for(int i = 0; i < spritesSpritesheet.size(); i++ ){
             Sprite sprite = spritesSpritesheet.getSprite(i);
             // TODO: Change these
-            float spriteWidth = sprite.getWidth() / 6;
-            float spriteHeight = sprite.getHeight() / 6;
+            float spriteWidth = sprite.getWidth();
+            float spriteHeight = sprite.getHeight();
             int id = sprite.getTexId();
             Vector2f[] texCoords = sprite.getTexCoords();
 
@@ -103,6 +109,29 @@ public class LevelEditorSceneInitializer extends SceneInitializer {
             if (i+1 < spritesSpritesheet.size() && nextButtonX2 < windowX2)
                 ImGui.sameLine();
        }
+        for(int i = 0; i < defaultTilesSpritesheet.size(); i++ ){
+            Sprite sprite = defaultTilesSpritesheet.getSprite(i);
+            // TODO: Change these
+            float spriteWidth = sprite.getWidth();
+            float spriteHeight = sprite.getHeight();
+            int id = sprite.getTexId();
+            Vector2f[] texCoords = sprite.getTexCoords();
+
+            ImGui.pushID(i);
+            if(ImGui.imageButton(id, spriteWidth, spriteHeight, texCoords[2].x, texCoords[0].y, texCoords[0].x, texCoords[2].y)){
+                GameObject object = Prefab.generateSpriteObject(sprite, 64, 64);
+                // Attach to mouse cursor
+                levelEditorStuff.getComponent(MouseControls.class).pickupObject(object);
+            }
+            ImGui.popID();
+
+            ImVec2 lastButtonPos = new ImVec2();
+            ImGui.getItemRectMax(lastButtonPos);
+            float  lastButtonX2 = lastButtonPos.x;
+            float  nextButtonX2 = lastButtonX2 + itemSpacing.x + spriteWidth;
+            if (i+1 < defaultTilesSpritesheet.size() && nextButtonX2 < windowX2)
+                ImGui.sameLine();
+        }
 
         ImGui.end();
     }
